b3fdc4fc00d79c72ad6326ca9776f13fd6e86f0f
raynertjx <rayner742@gmail.com>
2023-02-12 00:24:15 +0800

    Modified imports

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,19 +1,19 @@
-import Cards from "./Cards";
+import Cards from "./cards/Cards";
 
 export default function Banner(props) {
-  return (
-    <div className="w-full mt-8 flex flex-row flex-wrap justify-around">
-      <div className="w-96 flex flex-col justify-center items-center mx-12">
-        <div>
-          <h2 className="text-xl font-semibold w-full my-4">
-            Welcome to CAPT
-            <br />
-            Open House 2023!
-          </h2>
-          <h3 className="text-yellow-400">Explore CAPT here.</h3>
+    return (
+        <div className="w-full mt-8 flex flex-row flex-wrap justify-around">
+            <div className="w-96 flex flex-col justify-center items-center mx-12">
+                <div>
+                    <h2 className="text-xl font-semibold w-full my-4">
+                        Welcome to CAPT
+                        <br />
+                        Open House 2023!
+                    </h2>
+                    <h3 className="text-yellow-400">Explore CAPT here.</h3>
+                </div>
+            </div>
+            <Cards />
         </div>
-      </div>
-      <Cards />
-    </div>
-  );
+    );
 }
